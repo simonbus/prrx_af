5ad9be4e3f1c99b4ccef7aa4c957adbfe3ca85df
simonbus <busola1@poczta.onet.pl>
2023-01-01 22:38:10 +0100

    Distribution plot

---
+++ scipts/distribution.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+import json
+import os
+import helper
+from matplotlib import pyplot as plt
+
+
+def calc_percentiles(df):
+    """Calculate percentiles of pRRx/pRRx% parameters in AF and SR, return dict.
+
+    Args:
+        df (pd.DataFrame): DF with values of pRRx/pRRx% parameters
+
+    Returns:
+        (dict): Dictionary with values of percentiles in AF and SR
+    """
+    params = helper.get_params(df)
+    # Parameter types (perc, ms)
+    groups = [group for group in params if len(params[group]) > 0]
+    perc_dict = {g: dict() for g in groups}
+    for group in groups:
+        features = [x[0] for x in params[group]]
+        perc_dict[group] = {f: {'AF': {}, 'SR': {}} for f in features}
+        # Calculate percentiles (0-100)
+        for i, feature in enumerate(features):
+            for is_af, rhythm, in ((0, 'SR'), (1, 'AF')):
+                val = df[df['is_af'] == is_af][feature].values
+                perc = list(np.arange(101)/100)
+                q = list(np.quantile(val, perc))
+                perc_dict[group][feature][rhythm] = {
+                    'centiles': perc,
+                    'values': q}
+    return perc_dict
+
+
+def plot_distr(perc_dict, db, x_sec, fig_dir):
+    """Plot medians, and 10-90 and 25-75 percentile ranges
+    of pRRx/pRRx% parameters in AF and SR
+
+    Args:
+        perc_dict (dict): Dictionary with parameter names and percentiles in AF and SR
+        db (str): Acronym of the database
+        x_sec (int): Length of RR sequence [s]
+        fig_dir: Write directory for images
+    """
+    for group in perc_dict.keys():
+        ax = helper.default_fig()
+        title = f'Quartiles, 10-th and 90-th percentile of {group}'
+        if group == 'pRRx':
+            xlabel = 'Threshold $x$ [ms]'
+        else:
+            xlabel = 'Threshold $x$ [%]'
+        features = list(perc_dict[group].keys())
+        x_thr = helper.get_x_thr(features)
+        for rhythm, color in (('SR', 'tab:blue'), ('AF', 'tab:orange')):
+            # Values of percentiles
+            val = np.array([perc_dict[group][f][rhythm]['values'] for f in features])
+            # Median line
+            p50 = val[:, 50]
+            helper.plot_line(
+                ax, xval=x_thr, yval=p50, label=rhythm, xlim=(0, x_thr[-1]),
+                ylim=(0, 100), xlabel=xlabel, ylabel='[%]',
+                title=title, color=color, marker='o', markersize=2,
+                linewidth=2)
+            # Bands: 25-75 and 10-90 percentile ranges
+            for perc, lw, alpha in [(25, 1, 0.6), (10, 1, 0.3)]:
+                p_low = val[:, perc]
+                p_high = val[:, 100-perc]
+                ax.plot(x_thr, p_low, '-o', markersize=1, color=color,
+                        alpha=alpha, linewidth=lw)
+                ax.plot(x_thr, p_high, '-o', markersize=1, color=color,
+                        alpha=alpha, linewidth=lw)
+                ax.fill_between(x_thr, p_low, p_high, alpha=alpha, color=color)
+        ax.legend()
+        fname = f"distr_{group}_{db}_vs_threshold_{x_sec}s.png"
+        helper.save_fig(fig_dir, fname)
+        plt.show()
+
+
+if __name__ == '__main__':
+    prrx_dir = '../data/processed'
+    db = 'ltafdb'
+    fig_dir = '../reports/images/distr'
+    x_sec = 60
+    df = pd.read_csv(os.path.join(prrx_dir, db, f"prrx_perc_{db}_{x_sec}s.csv"))
+    perc_dict = calc_percentiles(df)
+    if not os.path.exists(fig_dir):
+        os.makedirs(os.path.join(fig_dir))
+    plot_distr(perc_dict, db, x_sec, fig_dir)
+    
---
+++ scipts/helper.py
@@ -0,0 +1,83 @@
+import re
+import os
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def get_params(df, col_name=None):
+    """Get grouped parameter names from pRRx dataframe
+
+    Args:
+        df (pd.DataFrame): DF with pRRx/pRRx% parameters as column names or a single column
+        col_name (str, optional): column with pRRx/pRRx% names. Defaults to None.
+
+    Returns:
+        (list): List of parameters
+    """
+    regex = {
+        'pRRx':    r'(?<=pRR)\d+.\d+(?![%\d])',
+        'pRRx%':   r'(?<=pRR)\d+\.*\d*(?=%)',
+    }
+    params = {}
+    if col_name is None:
+        lst = df.columns
+    else:
+        lst = df[col_name]
+    for item in lst:
+        for key in regex:
+            x = re.search(regex[key], item)
+            if x is not None:
+                x = x.group()
+                if key in params:
+                    params[key].append((item, x))
+                else:
+                    params[key] = [(item, x)]
+    return params
+
+
+def default_fig():
+    """Make a default figure
+
+    Returns:
+        plt.axis: Axis of a single-axis figure
+    """
+    fig, ax = plt.subplots(figsize=(8, 5))
+    return ax
+
+
+def get_x_thr(lst):
+    """Get thresholds from a list of pRRx/pRRx% parameters
+
+    Args:
+        lst (list): List of pRRx/pRRx% parameters
+
+    Returns:
+        (np.array): Thresholds (ms or %)
+    """
+    return np.array([float(re.search(r'\d+(\.\d+)*', f).group()) for f in lst])
+
+
+def plot_line(ax, xval, yval, label=None, xlim=None, ylim=None,
+              xlabel=None, ylabel=None, title=None, color=None,
+              marker=None, markersize=1, linewidth=1):
+    ax.plot(xval, yval, color=color, marker=marker, label=label,
+            markersize=markersize, linewidth=linewidth)
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    ax.minorticks_on()
+    ax.grid(b=True, which='major', linestyle='-')
+    ax.grid(b=True, which='minor', linestyle='--', linewidth=0.5)
+
+
+def save_fig(fig_dir, fname):
+    """Save figure to a file with specified name and directory
+
+    Args:
+        fig_path (str): Write directory
+        fname (str): Filename
+    """
+    plt.savefig(os.path.join(fig_dir, fname),
+                bbox_inches="tight", dpi=300)
